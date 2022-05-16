ed10932f4f85071612ac3295617b213edf7a618f
sugaryoleh <sugaryoleh@gmail.com>
2022-05-16 09:41:41 -0500

    creating Log entity

--- eld_admin/models.py
+++ eld_admin/models.py
@@ -1,17 +1,13 @@
 from django.db.models import Model, IntegerField, PositiveSmallIntegerField, CharField, EmailField, \
-    BooleanField, OneToOneField, SET_NULL, ForeignKey, RESTRICT
+    BooleanField, OneToOneField, SET_NULL, ForeignKey, RESTRICT, BigIntegerField, TimeField, DateField, CASCADE
+
 from phonenumber_field.modelfields import PhoneNumberField
+from address.models import AddressField, State, Locality
 
 from .validators import *
 
 
-class Province(Model):
-    id = IntegerField(primary_key=True)
-    name = CharField(max_length=100)
-    abbreviation = CharField(max_length=2, unique=True)
-
-
-class Group(Model):
+class UnitGroup(Model):
     id = IntegerField(primary_key=True)
     name = CharField(max_length=50)
     description = CharField(max_length=500)
@@ -24,9 +20,11 @@ class Unit(Model):
     make = CharField(max_length=50)
     model = CharField(max_length=50)
     license_plate_no = CharField(max_length=20, unique=True)
-    license_plate_state = ForeignKey(Province, on_delete=RESTRICT)
+    license_plate_state = ForeignKey(State, on_delete=RESTRICT)
     VIN = CharField(unique=True, validators=[validate_VIN])
-    group = ForeignKey(Group, on_delete=RESTRICT)
+    group = ForeignKey(UnitGroup, on_delete=RESTRICT)
+
+    # TODO: add files
 
 
 class Truck(Unit):
@@ -37,11 +35,6 @@ class Trailer(Unit):
     pass
 
 
-class Address:
-    pass
-    # TODO: create field
-
-
 class Driver(Model):
     id = IntegerField(primary_key=True)
     first_name = CharField(max_length=35)
@@ -49,9 +42,50 @@ class Driver(Model):
     last_name = CharField(max_length=35)
     email = EmailField()
     phone = PhoneNumberField(unique=True)
-    homeTerminalAddress = ForeignKey(Address, on_delete=RESTRICT)
+    homeTerminalAddress = ForeignKey(AddressField, on_delete=RESTRICT)
     eldEnabled = BooleanField()
     truck = OneToOneField(Truck, on_delete=SET_NULL)
     trailer = OneToOneField(Trailer, on_delete=SET_NULL)
     notes = CharField(max_length=500)
+
+    # TODO: add file field
+
+
+class Log(Model):
+    id = BigIntegerField(primary_key=True)
+    date = DateField()
+    shipping_docs_uploaded = BooleanField()
+    DVIR_completed = BooleanField()
+
+    # TODO: add file field
+
+
+class LogProfile(Model):
+    id = OneToOneField(Log, on_delete=RESTRICT, primary_key=True)
+    driver = ForeignKey(Driver, on_delete=RESTRICT)
+    fromAddress = ForeignKey(Locality, on_delete=RESTRICT)
+    toAddress = ForeignKey(Locality, on_delete=RESTRICT)
+    distance = PositiveSmallIntegerField()
+    notes = CharField(max_length=50)
+
+    # TODO: add file field
+
+
+class Status(Model):
+    id = PositiveSmallIntegerField(primary_key=True)
+    name = CharField(max_length=10)
+    note = CharField(max_length=50)
+    optional = BooleanField()
+
+
+class LogEvent(Model):
+    id = BigIntegerField(primary_key=True)
+    log = ForeignKey(Log, on_delete=CASCADE)
+    status = ForeignKey(Status, on_delete=RESTRICT)
+    start_time = TimeField()
+    end_time = TimeField()
+    truck = ForeignKey(Truck, on_delete=RESTRICT)
+    odometer_value = PositiveSmallIntegerField()
+    notes = CharField(max_length=40)
+
 
